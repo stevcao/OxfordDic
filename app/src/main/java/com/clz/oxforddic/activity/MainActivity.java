0947377f0143452014d11b3db58cebb0aee4c167
stevcao <stevcao@tencent.com>
2019-04-30 09:10:51 +0800

    收藏的资源添加

--- app/src/main/java/com/clz/oxforddic/activity/MainActivity.java
+++ app/src/main/java/com/clz/oxforddic/activity/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Window;
-import android.view.WindowManager;
 
 import com.clz.oxforddic.R;
 import com.clz.oxforddic.ui.TabFactory;
